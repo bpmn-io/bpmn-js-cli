a477c52e8d992adf6626c954ff52b09f240dc2bc
Nico Rehwaldt <git_nikku@nixis.de>
2014-08-01 08:34:24 +0200

    feat(plug-in): add shape and save commands

--- test/spec/cliSpec.js
+++ test/spec/cliSpec.js
@@ -1,9 +1,12 @@
 'use strict';
 
-var TestHelper = require('bpmn-js/test/spec/TestHelper');
+var TestHelper = require('bpmn-js/test/TestHelper'),
+    Matchers = require('bpmn-js/test/Matchers');
 
 /* global bootstrapBpmnJS, inject */
 
+
+var _ = require('lodash');
 
 var fs = require('fs');
 
@@ -14,16 +17,70 @@ var modelingModule = require('bpmn-js/lib/features/modeling'),
 
 describe('cli', function() {
 
+  beforeEach(Matchers.addDeepEquals);
+
 
   var diagramXML = fs.readFileSync('resources/start.bpmn', 'utf-8');
 
   var testModules = [ drawModule, cliModule, modelingModule ];
 
-  beforeEach(bootstrapBpmnJS(diagramXML, { config: { cli: { bindTo: 'cli' } }, modules: testModules }));
+  beforeEach(bootstrapBpmnJS(diagramXML, {
+    modules: testModules,
+    cli: { bindTo: 'cli' }
+  }));
+
+
+  it('should bind to window', inject(function(cli) {
+    expect(window.cli).toBeDefined();
+  }));
 
 
   it('should model, cli based', inject(function(cli) {
-    expect(window.cli).toBeDefined();
+
+    // shapes
+    var shapes = cli.shapes('shapes');
+
+    // then
+    expect(shapes).toDeepEqual([ 'StartEvent_1', 'StartEvent_1_label' ]);
+
+
+    // shape
+    var startEventElement = cli.shape('StartEvent_1');
+
+    // then
+    expect(startEventElement.id).toBe('StartEvent_1');
+
+
+    // move
+    var startEventPos = _.pick(startEventElement, [ 'x', 'y' ]);
+    cli.move('StartEvent_1 0,50');
+
+    // then
+    expect(startEventElement.x).toBe(startEventPos.x);
+    expect(startEventElement.y).toBe(startEventPos.y + 50);
+
+
+    // append
+    var task = cli.append('StartEvent_1 bpmn:ServiceTask');
+
+    // then
+    expect(task).toBeDefined();
+
+
+    // append another
+    var otherTask = cli.append(task.id + ' bpmn:UserTask 150,0');
+
+    // then
+    expect(otherTask).toBeDefined();
+
+
+    // export as svg
+    cli.save('svg');
+
+
+    // export as bpmn
+    cli.save('bpmn');
+
   }));
 
 });
