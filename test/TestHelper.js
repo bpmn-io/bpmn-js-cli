8881843c1b339ea91ecff93d2dfde345a85a7df9
Martin Stamm <martin.stamm@camunda.com>
2022-02-28 09:21:46 +0100

    CI: integration test with `bpmn-js@8`

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -1,11 +1,41 @@
 export * from 'bpmn-js/test/helper';
 
+import semver from 'semver';
+
 import {
   insertCSS
 } from 'bpmn-js/test/helper';
+
 
 // insert diagram.css
 insertCSS(
   'diagram-js.css',
   require('diagram-js/assets/diagram-js.css')
 );
+
+if (bpmnJsSatisfies('>=9')) {
+  insertCSS(
+    'bpmn-js.css',
+    require('bpmn-js/dist/assets/bpmn-js.css').default
+  );
+}
+
+/**
+ * Execute test only if currently installed bpmn-js is of given version.
+ *
+ * @param {string} versionRange
+ * @param {boolean} only
+ */
+export function withBpmnJs(versionRange, only = false) {
+  if (bpmnJsSatisfies(versionRange)) {
+    return only ? it.only : it;
+  } else {
+    return it.skip;
+  }
+}
+
+function bpmnJsSatisfies(versionRange) {
+  const bpmnJsVersion = require('bpmn-js/package.json').version;
+
+  return semver.satisfies(bpmnJsVersion, versionRange, { includePrerelease: true });
+}
